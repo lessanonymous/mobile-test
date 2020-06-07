a24d3b3970f6749f608afb3a84abc298be60301f
lessanonymous <63210198+lessanonymous@users.noreply.github.com>
2020-06-07 02:54:47 +0200

    added main grid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h2>Header</h2>
       </header>
+      <main>
+        <h2>Main</h2>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis
+          incidunt mollitia ex. Sed voluptatem quidem harum blanditiis pariatur
+          odit illum dolore repudiandae, corrupti quis provident veniam neque
+          asperiores sunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis
+          incidunt mollitia ex. Sed voluptatem quidem harum blanditiis pariatur
+          odit illum dolore repudiandae, corrupti quis provident veniam neque
+          asperiores sunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis
+          incidunt mollitia ex. Sed voluptatem quidem harum blanditiis pariatur
+          odit illum dolore repudiandae, corrupti quis provident veniam neque
+          asperiores sunt?
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum omnis
+          incidunt mollitia ex. Sed voluptatem quidem harum blanditiis pariatur
+          odit illum dolore repudiandae, corrupti quis provident veniam neque
+          asperiores sunt?
+        </p>
+      </main>
+      <footer>
+        <h2>Footer</h2>
+      </footer>
     </div>
   );
 }
